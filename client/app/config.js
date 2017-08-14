a16e68af71899abfbf3dcd919fdf8274ecbbf98d
orbartal <orbartal@gmail.com>
2017-08-14 01:05:34 -0400

    Add third parties libraries using bower. Add app, config, and the first state page.

---
+++ client/app/config.js
@@ -0,0 +1,22 @@
+﻿(function () {
+    'use strict';
+
+     angular.module('wikiApp').
+     	config(config).
+     	run(['$rootScope', '$state', run]);
+
+     function run($rootScope, $state){
+    	 $rootScope.$state = $state;
+     }//End run
+
+    //Config
+    function config($stateProvider, $urlRouterProvider) {
+    	$urlRouterProvider.otherwise("/home");
+    	$stateProvider
+
+	    	.state('home', {
+	            url: "/home",
+	            templateUrl: "/app/pages/home/home.view.html"
+	        })
+    }
+})();
