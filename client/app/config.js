850bdbc590fcda3956bb47bea3c25859c6108ed9
orbartal <orbartal@gmail.com>
2017-08-14 12:58:33 -0400

    Add frame to the site.

--- client/app/config.js
+++ client/app/config.js
@@ -14,7 +14,13 @@
     	$urlRouterProvider.otherwise("/home");
     	$stateProvider
 
-	    	.state('home', {
+      .state('site', {
+	            url: "",
+	            templateUrl: "/app/common/frame/wiki.frame.view.html",
+	            controller: "FrameCtrl as vm",
+	     })
+
+	    	.state('site.home', {
 	            url: "/home",
 	            templateUrl: "/app/pages/home/home.view.html",
               controller: "HomeCtrl as vm",
