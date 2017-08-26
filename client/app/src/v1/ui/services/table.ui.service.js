afeffe873e9adc459ca42b58cff032b14ae321e2
orbartal <orbartal@gmail.com>
2017-08-26 10:09:05 -0400

    Fix bug in passing the correct row data to the modal.

--- client/app/src/v1/ui/services/table.ui.service.js
+++ client/app/src/v1/ui/services/table.ui.service.js
@@ -50,11 +50,14 @@
         function makeCell (colIndex, rowIndex, data, options) {
             var result = 'error';
             var col = options.tableCols[colIndex];
+            var row = data[rowIndex];
             if ("actions"===col.field){
-                 result =  '<buttons-in-row data="data['+rowIndex+']" options="options.rowButtons">'
+                 var rowJson = angular.toJson(row);
+                 result =  //'<p>' + row ['id']+'</p>' +
+                            //'<buttons-in-row data="data['+rowIndex+']" options="options.rowButtons">'
+                            '<buttons-in-row data=\''+rowJson +'\' options="options.rowButtons">'
                             +'</buttons-in-row>';
             }else{
-                var row = data[rowIndex];
                 result = '<p>' + row [col.field]+'</p>';
             }
             return result;
